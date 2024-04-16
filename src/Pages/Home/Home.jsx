8957cbf46b6e61770237284590b65c359d68ddfc
faria-r <fariacse11@gmail.com>
2024-04-16 22:20:54 +0600

    initialized college project with primary setup

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <p className='text-green-600 text-3xl'>Welcome Back</p>
+        </div>
+    );
+};
+
+export default Home;
